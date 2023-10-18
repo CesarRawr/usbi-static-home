dc26f0e27f4ba898ff469e446713a1ca8efe14f3
CesarRawr <rawrcesar13@gmail.com>
2023-10-18 11:34:54 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Ruta para servir archivos estáticos (HTML, CSS, JS) desde el directorio actual
+app.use(express.static(path.join(__dirname + "/src")));
+
+// Ruta para manejar la solicitud del archivo HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/src", 'homePage.html'));
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
