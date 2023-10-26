96a77c54c1aaa51ee7db66e00e67928ed5bd6941
CesarRawr <rawrcesar13@gmail.com>
2023-10-26 16:08:35 -0600

    Added favicon and credits html

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/src", 'homePage.html'));
 });
 
+app.get('/creditos', (req, res) => {
+    res.sendFile(path.join(__dirname + "/src", 'credits.html'));
+});
+
 // Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
